6464c7486a57062067dc65564337281ecb0bac5b
Adam Black <fernando@notoriovs.com>
2025-10-04 09:50:13 -0600

    First

---
+++ info.js
@@ -0,0 +1,31 @@
+export const info = {
+  legalName: 'AM Dental Studio',
+  companyName: 'AM Dental Studio',
+  description: 'Diseño de Sonrisas',
+  email: {
+    sender: 'ventas@amdental.com',
+    recipients: ['ventas@amdental.com'],
+    subject: 'Nuevo prospecto AM Dental',
+  },
+  phoneNumber: '+',
+  whatsapp: {
+    value: '+',
+    message: 'Buen día, me interesa un diseño de sonrisa!',
+  },
+  social: {
+    facebook: '',
+    instagram: '',
+  },
+  address: {
+    address: '',
+    col: '',
+    cp: '',
+    city: '',
+    state: '',
+  },
+  surveyWebhook: '',
+  oldSurveyWebhook: '',
+  schedulerLink: '',
+  privacyNotice: '/aviso-privacidad',
+  termsConditions: '/aviso-privacidad',
+};
---
+++ src/pages/results.js
@@ -0,0 +1,148 @@
+import { getCookie } from 'cookies-next';
+import Image from 'next/image';
+import { Select } from '../components/form/formAtoms';
+import { FormProvider, useForm } from 'react-hook-form';
+import Blockbuster from '../components/blockbuster';
+import i00 from '../../public/results/00.jpg';
+import iTestimonios from '../../public/survey/03.jpg';
+import iBeneficios from '../../public/survey/02.jpg';
+import iFaqs from '../../public/survey/05.jpg';
+import { info } from '../../info';
+import { useState } from 'react';
+import Faqs from '../components/faqs';
+
+export default function Results({lead}) {
+  const [tienda, setTienda] = useState('');
+  const methods = useForm({mode: 'all'});
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+    watch,
+  } = methods;
+
+  const {fullName} = lead;
+  const firstName = fullName.split(' ')[0];
+
+  const SectionCTA = () => <div className="w-full space-y-4">
+    <hr className="mb-8"/>
+    <p className="ft-4 text-center font-semibold">Tienes alguna pregunta?</p>
+    <a href="" target="_blank" className="button !w-full">Mándanos un WhatsApp</a>
+  </div>;
+
+  return (
+    <div className="relative flex flex-col flex-grow bg-gradient-to-t from-blue-50 to-white">
+      <div
+        className="relative container !px-0 md:pb-0 flex flex-col flex-grow md:flex-grow-0 items-center pointer-events-auto touch-auto">
+        <div className="survey-card border-b pb-12">
+          <div className="w-full py-12 space-y-12">
+            <h1><span className="font-semibold">{firstName ?? "Hey"}</span>, tu sonrisa merece un plan hecho para ti
+            </h1>
+            <div className="relative w-full aspect-[4/3] rounded-2xl overflow-hidden">
+              <Image src={i00} layout="fill" objectFit="cover"/>
+            </div>
+            <p className="ft-2">Hola, soy Ale Mora, cirujana dentista con
+              especialidad en ortodoncia por la Universidad Autónoma de Guadalajara. <br/><br/>
+              En AM Dental Studio dirijo un
+              equipo increíble y me apasiona ayudar
+              a que cada persona se sienta cómoda y
+              feliz con su sonrisa.</p>
+          </div>
+
+
+          <SectionCTA/>
+
+        </div>
+      </div>
+
+      <Blockbuster
+        overhead="Testimonios"
+        title="Pacientes que ya confiaron en nosotros."
+        background={iTestimonios}
+      />
+      <section className="container my-20">
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-20">
+          <div className="relative">
+            <div className="relative w-full aspect-[1/.88] overflow-hidden">
+              <Image src="/survey/Testimonios_01.png" layout="fill" objectFit="cover" objectPosition="top"/>
+            </div>
+            <div className="relative w-full aspect-[1/.97] overflow-hidden">
+              <Image src="/survey/Testimonios_02.png" layout="fill" objectFit="cover" objectPosition="top"/>
+            </div>
+            <div className="relative w-full aspect-[1/1.2] overflow-hidden mb-4">
+              <Image src="/survey/Testimonios_03.png" layout="fill" objectFit="cover" objectPosition="top"/>
+            </div>
+          </div>
+        </div>
+        <SectionCTA/>
+      </section>
+
+      <Blockbuster
+        overhead="Beneficios"
+        title="La diferencia está en como te cuidamos"
+        background={iBeneficios}
+      />
+
+      <section className="container my-20">
+        <div className="gap-8 grid grid-cols-1 mb-20">
+          <div className="p-6 rounded-xl bg-brand-2 border-2 border-brand-2 shadow-md">
+            <p className="font-semibold ft-4 text-center">+500 sonrisas transformadas</p>
+          </div>
+          <div className="p-6 rounded-xl bg-brand-2 border-2 border-brand-2 shadow-md">
+            <p className="font-semibold ft-4 text-center">Atención 24/7 por WhatsApp</p>
+          </div>
+          <div className="p-6 rounded-xl bg-brand-2 border-2 border-brand-2 shadow-md">
+            <p className="font-semibold ft-4 text-center">Diagnóstico con IA</p>
+          </div>
+          <div className="p-6 rounded-xl bg-brand-2 border-2 border-brand-2 shadow-md">
+            <p className="font-semibold ft-4 text-center">Opciones de pago accesibles</p>
+          </div>
+        </div>
+        <SectionCTA/>
+      </section>
+
+      <Blockbuster
+        overhead="FAQs"
+        title="¿Dudas sobre tu tratamiento? probablemente estén aquí"
+        background={iFaqs}
+      />
+
+      <section className="container my-20">
+        <Faqs />
+        <SectionCTA/>
+      </section>
+
+    </div>
+  );
+}
+
+export async function getServerSideProps(ctx) {
+  const {req, res} = ctx;
+  const leadCookie = getCookie('lead', {req, res}) || '{}';
+  const _fbc = getCookie('_fbc') || '';
+  const _fbp = getCookie('_fbp') || '';
+
+  const lead = JSON.parse(leadCookie);
+
+  if (!lead || lead === 'null' || Object.keys(lead).length === 0) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/',
+      },
+    };
+  }
+
+  return {
+    props: {
+      lead: {
+        fullName: lead.fullName,
+        phone: lead.phone,
+        whatsapp: lead.whatsapp,
+        sheetRow: lead.sheetRow || '',
+        _fbc,
+        _fbp,
+      },
+    },
+  };
+}
